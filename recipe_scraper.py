0946bd5d8be451657d5b5c77d169b926f68452fa
Musiki8213 <sithomolamusiki@gmail.com>
2025-07-25 15:47:21 +0200

    first commit

---
+++ recipe_scraper.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+#opening the webpage
+
+response = requests.get("https://test-scrape-site.onrender.com/recipes.html")
+
+#Reading the HTML
+soup = BeautifulSoup(response.text, "html.parser")
+recipes = soup.find_all("div", class_="recipe")
+
+
+# Creating an Excel workbook
+wb = Workbook()
+ws = wb.active
+ws.title = "Recipes"
+
+#Header row
+ws.append(["Name", "Ingredients", "Instructions"])
+
+
+#Extract and write recipe data
+
+for r in recipes:
+    name = r.find("h2").text
+    ingredients = [li.text for li in r.find("ul").find_all("li")]
+    instructions = r.find("p").text
+
+    # Join ingredients into a single string
+    ingredients_text = ", ".join(ingredients)
+
+    # Add row to Excel
+    ws.append([name, ingredients_text, instructions])
+
+    # Save Excel file
+    wb.save("scrape_recipes.xlsx")
